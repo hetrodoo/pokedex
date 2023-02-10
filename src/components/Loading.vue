a159b7f6dbd2b9b826b59090d4aa91f7fce1c558
hetrodoo <eduleite62@gmail.com>
2023-02-10 06:57:46 -0300

    Added more primitives.

---
+++ src/components/Loading.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="loader-container">
+    <div class="el"></div>
+    <div class="el"></div>
+    <div class="el"></div>
+    <div class="el"></div>
+    <div class="el"></div>
+    <div class="el"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class Loading extends Vue {
+}
+</script>
+
+<style lang="sass" scoped>
+.loader-container
+  display: inline-flex
+  gap: 8px
+  height: 32px
+  justify-content: center
+  align-items: center
+
+  .el
+    width: 8px
+    height: 8px
+    border-radius: 8px
+    background-color: #2c3e50
+    animation: loadingAnim ease-in-out 600ms infinite
+
+  .el:nth-child(1)
+    animation-delay: 50ms
+
+  .el:nth-child(2)
+    animation-delay: 100ms
+
+  .el:nth-child(3)
+    animation-delay: 150ms
+
+  .el:nth-child(4)
+    animation-delay: 200ms
+
+  .el:nth-child(5)
+    animation-delay: 250ms
+
+  .el:nth-child(6)
+    animation-delay: 300ms
+
+@keyframes loadingAnim
+  0%
+    height: 8px
+  50%
+    height: 24px
+  100%
+    height: 8px
+</style>
